ef4f433aade919e4e4b5832205102ddc34566951
lukejlackey <lukejlackey@gmail.com>
2022-09-12 14:12:40 -0700

    initial

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8000;
 require('./config/mongoose.config');
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
